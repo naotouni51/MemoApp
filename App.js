dbc3036f2eb41e52d575ee6f34a660f08e84a0c9
naotouni51 <naotouni51@gmail.com>
2019-07-16 06:36:19 +0900

    Create Login and Signup Screen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, View, Platform } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 
 export default class App extends React.Component {
@@ -10,7 +10,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoEditScreen />
+        <SignupScreen />
       </View>
     );
   }
