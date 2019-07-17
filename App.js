373c502529606aeb55922a4d9fdaafdb5706a0f3
naotouni51 <naotouni51@gmail.com>
2019-07-18 03:38:11 +0900

    Install react-navigation

--- App.js
+++ App.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import { StyleSheet, View, Platform } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-import Appbar from './src/components/Appbar';
+import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
+const App = createStackNavigator({
+  Home:       { screen: MemoListScreen },
+  MemoDetail: { screen: MemoDetailScreen },
+  MemoEdit:   { screen: MemoEditScreen },
+  Login:      { screen: LoginScreen },
+  Signup:     { screen: SignupScreen },
+}, {
+  defaultNavigationOptions: {
+    headerTitle: 'Memot',
+    headerStyle: {
+      backgroundColor: '#265366',
+    },
+    headerTitleStyle: {
+      flex: 1,
+      color: '#fff',
+      textAlign: 'center',
+    },
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Appbar />
-        <SignupScreen />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 78,
-    backgroundColor: '#FFFDF6',
   },
 });
+
+export default createAppContainer(App);
